9d13f4fbb0e702ca4eddee85376ec61a3b0ccbff
Nishant <entrocomedy@gmail.com>
2024-10-24 13:32:03 +0530

    first

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
-import TodoList from './todoList/Todolist';
+// import TodoList from './todoList/Todolist';
+import Weather from './weather/Weather';
 
 function App() {
   return (
     <div className="App">
-      <TodoList />
+      {/* <TodoList /> */}
+      <Weather />
     </div>
   );
 }
